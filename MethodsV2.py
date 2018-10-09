4bae21a609cd10f0b77ffec72700d464718fd173
Isaac Gluck <isaacbgluck@gmail.com>
2018-10-08 23:25:01 -0400

    finished first two methods

--- MethodsV2.py
+++ MethodsV2.py
@@ -17,4 +17,15 @@ def readTrees(filenames):
 def makeQuartetDictionary(tree_list):
     taxon_list = tree_list.taxon_namespace.get_taxa
     combinations_of_taxa = combinations(taxon_list)
-    
+
+    dictonary_of_quartets = {}
+
+    for tuple_of_leaves in combinations_of_taxa:
+        topology1 = "(({0},{1}),{2},{3});".format(tuple_of_leaf_names[0], tuple_of_leaf_names[1], tuple_of_leaf_names[2], tuple_of_leaf_names[3])
+		topology2 = "(({0},{1}),{2},{3});".format(tuple_of_leaf_names[0], tuple_of_leaf_names[2], tuple_of_leaf_names[1], tuple_of_leaf_names[3])
+		topology3 = "(({0},{1}),{2},{3});".format(tuple_of_leaf_names[0], tuple_of_leaf_names[3], tuple_of_leaf_names[1], tuple_of_leaf_names[2])
+        dictonary_of_quartets[tuple_of_leaves] = [Tree.get(data=topology1, schema="newick"), 0,
+                                                  Tree.get(data=topology2, schema="newick"), 0,
+                                                  Tree.get(data=topology3, schema="newick"), 0]
+
+    return dictonary_of_quartets

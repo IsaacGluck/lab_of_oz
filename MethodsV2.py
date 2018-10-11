fcf1ec36c1a02f0eed4ae7e6be20b18ce8a0c194
Isaac Gluck <isaacbgluck@gmail.com>
2018-10-11 14:02:14 -0400

    filling dict

--- MethodsV2.py
+++ MethodsV2.py
@@ -30,16 +30,19 @@ sample_tree_list = readTrees(['testTree.txt'])
 # Input: Dendropy TreeList object
 # Output: Quartet dictionary with all unique quartets from the tree_list
 def makeQuartetDictionary(tree_list):
-    taxon_list = tree_list.taxon_namespace.labels()
-    combinations_of_taxa = combinations(taxon_list, 4)
+    taxon_label_list = tree_list.taxon_namespace.labels()
+    combinations_of_taxa = combinations(taxon_label_list, 4)
 
     dictonary_of_quartets = {}
 
     for tuple_of_leaves in combinations_of_taxa:
-        topology1 = "(({0},{1}),({2},{3}));".format(tuple_of_leaves[0], tuple_of_leaves[1], tuple_of_leaves[2], tuple_of_leaves[3])
-        topology2 = "(({0},{1}),({2},{3}));".format(tuple_of_leaves[0], tuple_of_leaves[2], tuple_of_leaves[1], tuple_of_leaves[3])
-        topology3 = "(({0},{1}),({2},{3}));".format(tuple_of_leaves[0], tuple_of_leaves[3], tuple_of_leaves[1], tuple_of_leaves[2])
-        dictonary_of_quartets[tuple_of_leaves] = [Tree.get(data=topology1, taxon_namespace=tree_list.taxon_namespace, schema="newick"), 0,
+        sorted_list_of_leaves = list(tuple_of_leaves)
+        sorted_list_of_leaves.sort()
+        frozenset_of_leaves = frozenset(sorted_list_of_leaves)
+        topology1 = "(({0},{1}),({2},{3}));".format(sorted_list_of_leaves[0], sorted_list_of_leaves[1], sorted_list_of_leaves[2], sorted_list_of_leaves[3])
+        topology2 = "(({0},{1}),({2},{3}));".format(sorted_list_of_leaves[0], sorted_list_of_leaves[2], sorted_list_of_leaves[1], sorted_list_of_leaves[3])
+        topology3 = "(({0},{1}),({2},{3}));".format(sorted_list_of_leaves[0], sorted_list_of_leaves[3], sorted_list_of_leaves[1], sorted_list_of_leaves[2])
+        dictonary_of_quartets[frozenset_of_leaves] = [Tree.get(data=topology1, taxon_namespace=tree_list.taxon_namespace, schema="newick"), 0,
                                                   Tree.get(data=topology2, taxon_namespace=tree_list.taxon_namespace, schema="newick"), 0,
                                                   Tree.get(data=topology3, taxon_namespace=tree_list.taxon_namespace, schema="newick"), 0]
 
@@ -47,29 +50,95 @@ def makeQuartetDictionary(tree_list):
 
 # Test makeQuartetDictionary
 quartet_dictionary = makeQuartetDictionary(sample_tree_list)
+# pprint(len(quartet_dictionary))
+
+
+# Input: Dendropy Tree object, a quartet dictionary as created by makeQuartetDictionary()
+# Output: The quartet dictionary with updated support vectors for that tree
+def getTreeQuartetSupport(tree, quartet_dictionary):
+    taxon_label_list = [(n.taxon.label) for n in tree.leaf_nodes()]
+    frozenset_of_taxa = frozenset(taxon_label_list) # unique set of all taxa
+
+    print(tree.as_ascii_plot())
+
+    for quartet in quartet_dictionary:
+        if quartet.issubset(frozenset_of_taxa): # if the tree contains the quartet
+            sorted_quartet = list(quartet)
+            sorted_quartet.sort()
+
+            single_tree_list = TreeList()
+            single_tree_list.append(tree.extract_tree_with_taxa_labels(quartet))
+
+            # print(sorted_quartet, single_tree_list.frequency_of_bipartition(labels=[sorted_quartet[0], sorted_quartet[1]]),
+            #                       single_tree_list.frequency_of_bipartition(labels=[sorted_quartet[0], sorted_quartet[2]]),
+            #                       single_tree_list.frequency_of_bipartition(labels=[sorted_quartet[0], sorted_quartet[3]]))
+
+            # Check 1st Topology
+            result0 = single_tree_list.frequency_of_bipartition(labels=[sorted_quartet[0], sorted_quartet[1]])
+            if (result0 > 0):
+                quartet_dictionary[quartet][1] = quartet_dictionary[quartet][1] + 1
+                continue
+
+            # Check 2nd Topology
+            result1 = single_tree_list.frequency_of_bipartition(labels=[sorted_quartet[0], sorted_quartet[2]])
+            if (result1 > 0):
+                quartet_dictionary[quartet][3] = quartet_dictionary[quartet][3] + 1
+                continue
+
+            # Check 3rd Topology
+            result2 = single_tree_list.frequency_of_bipartition(labels=[sorted_quartet[0], sorted_quartet[3]])
+            if (result2 > 0):
+                quartet_dictionary[quartet][5] = quartet_dictionary[quartet][5] + 1
+                continue
+
+            # ERROR
+            print("Error: Topology is not a match")
+            print(quartet, quartet_dictionary[quartet])
+            break
+
+
+getTreeQuartetSupport(sample_tree_list[0], quartet_dictionary)
+# getTreeQuartetSupport(sample_tree_list[1], quartet_dictionary)
 # pprint(quartet_dictionary)
 
 
-# Input: Dendropy TreeList object, a quartet dictionary as created by makeQuartetDictionary()
-# Output: The quartet dictionary with filled support vectors
-def fillQuartetSupportVectors(tree_list, quartet_dictionary):
-    for quartet in quartet_dictionary:
-        # print((quartet[0], quartet[1]), (quartet[0], quartet[2]), (quartet[0], quartet[3]))
-        support_vector = quartet_dictionary[quartet]
-        print(quartet, support_vector[0], support_vector[2], support_vector[4])
-        print(tree_list.frequency_of_bipartition(labels=[quartet[0], quartet[1]]))
-        print(tree_list.frequency_of_bipartition(labels=[quartet[0], quartet[1], quartet[2], quartet[3]]))
+
+
+
+
+
+
+
+
+
+
+# A - trees with duo (freq * number trees)
+# B - trees with quad
+# A >= B
+
+# 100 trees total
+# 50 trees have the quad
+# 70 have the split
+
+
+    # for quartet in quartet_dictionary:
+    #     # print((quartet[0], quartet[1]), (quartet[0], quartet[2]), (quartet[0], quartet[3]))
+    #     support_vector = quartet_dictionary[quartet]
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #     print(quartet, support_vector[0], support_vector[2], support_vector[4])
+    #     print(tree_list.frequency_of_bipartition(labels=[quartet[0], quartet[1]]))
+    #     print(tree_list.frequency_of_bipartition(labels=[quartet[0], quartet[1], quartet[2], quartet[3]]))
         # for tree in tree_list:
         #     support_vector = quartet_dictionary[quartet]
         #     print(quartet, support_vector[0], support_vector[2], support_vector[4])
-
-
-
-
-
-
-
-
 
 
         #     pruned_tree = tree.extract_tree_with_taxa_labels(quartet)
@@ -100,11 +169,11 @@ def fillQuartetSupportVectors(tree_list, quartet_dictionary):
         #         # print("Error: Topology is not a match")
         #         # print(quartet, quartet_dictionary[quartet])
         #         # break
-        break
-    return quartet_dictionary
+        # break
+    # return quartet_dictionary
 
 # Test fillQuartetSupportVectors
-quartet_dictionary = fillQuartetSupportVectors(sample_tree_list, quartet_dictionary)
+# quartet_dictionary = fillQuartetSupportVectors(sample_tree_list, quartet_dictionary)
 # print(quartet_dictionary)
 
 
